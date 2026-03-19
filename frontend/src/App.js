bf48b961846813afbeea2d2828abf54af53d035b
Vipin Kaushik <vipinkaushik006@gmail.com>
2026-03-19 17:56:27 +0530

    Fix routing blank screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext';
